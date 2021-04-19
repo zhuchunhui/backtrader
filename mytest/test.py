0e9e36ca6f0ba2f6d194a843bdc136e4b8cf413c
zhuchunhui <zhuch.cool@163.com>
2021-04-20 00:32:47 +0800

    20210419-提交阶段性版本

--- mytest/test.py
+++ mytest/test.py
@@ -1,4 +1,13 @@
 from datetime import datetime
+import time
+from datetime import date
 
-dt = datetime.strptime('2021-01-29 09:00:00+00:00','%Y-%m-%d %H:%M:%S+00:00')
-print(dt.date())
+# dt = datetime.strptime('2021-01-29 09:00:00+00:00','%Y-%m-%d %H:%M:%S+00:00')
+# print(dt.date())
+# print(time.time())
+
+timeArray = time.localtime(int(1617640758))
+print(time.strftime("%Y-%m-%d %H:%M:%S", timeArray))
+
+# stamp=time.mktime(time.strptime('2016-11-24 14:00:21', '%Y-%m-%d %H:%M:%S'))
+# print(int(stamp))
